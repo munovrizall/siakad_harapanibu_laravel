8f72807d13fb8c3eabc10fe60de81f2fc5957962
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-04-05 20:06:13 +0700

    adjusting sidebar position

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -8,17 +8,25 @@
         </div>
 
         <ul class="sidebar-menu">
-            
+
             <li class="{{ Request::is('admin-home') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('/admin-home') }}"><i class="fas fa-home"></i> <span>Home</span></a>
             </li>
-            
-            <li class="{{ Request::is('guru*') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ url('/guru') }}"><i class="fas fa-chalkboard-user"></i> <span>Data Guru</span></a>
+
+            <li class="{{ Request::is('jadwal-pelajaran*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('/jadwal-pelajaran') }}"><i class="fas fa-calendar-week"></i> <span>Jadwal Pelajaran</span></a>
             </li>
-           
+
+            <li class="{{ Request::is('mata-pelajaran*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('/mata-pelajaran') }}"><i class="fas fa-book"></i> <span>Mata Pelajaran</span></a>
+            </li>
+
             <li class="{{ Request::is('siswa*') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('/siswa') }}"><i class="fas fa-user-group"></i> <span>Data Siswa</span></a>
+            </li>
+
+            <li class="{{ Request::is('guru*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('/guru') }}"><i class="fas fa-chalkboard-user"></i> <span>Data Guru</span></a>
             </li>
 
             <li class="{{ Request::is('kelas*') ? 'active' : '' }}">
@@ -28,19 +36,10 @@
             <li class="{{ Request::is('jurusan*') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('/jurusan') }}"><i class="fas fa-school"></i> <span>Data Jurusan</span></a>
             </li>
-            
-            <li class="{{ Request::is('mata-pelajaran*') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ url('/mata-pelajaran') }}"><i class="fas fa-book"></i> <span>Mata Pelajaran</span></a>
-            </li>
-            
-            <li class="{{ Request::is('jadwal-pelajaran*') ? 'active' : '' }}">
-                <a class="nav-link" href="{{ url('/jadwal-pelajaran') }}"><i class="fas fa-calendar-week"></i> <span>Jadwal Pelajaran</span></a>
-            </li>
 
             <li class="{{ Request::is('akun*') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ url('/akun') }}"><i class="fas fa-user-gear"></i> <span>Akun Pengguna</span></a>
             </li>
-            
         </ul>
 
     </aside>
